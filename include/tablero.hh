dc8e7df819996efcee4d496e158532adc1045724
AaronUCR <aaron.arcealfaro@ucr.ac.cr>
2024-05-27 01:48:00 -0600

    Solucion error de include y to do en tablkero.cpp

---
+++ include/tablero.hh
@@ -0,0 +1,23 @@
+#ifndef TABLERO_HH
+#define TABLERO_HH
+#include <iostream>
+#include<string>
+#include <vector>
+using namespace std;
+class Tablero {
+    public:
+        // Constructores
+        Tablero(int filas, int columnas);
+        // Metodos
+        bool puedeTirar(int columna);
+        bool soltarFicha(char ficha);
+        char mostrarTablero();
+        int getColumnas();
+        int getFilas();
+        vector<vector<char>> getTablero();
+    private:
+        vector<vector<char>> tablero;
+        int filas;
+        int columnas;
+    };
+#endif
--- src/tablero.cpp
+++ src/tablero.cpp
@@ -22,29 +22,3 @@ bool Tablero::puedeTirar(int columna){
     }
     return true;
 }
-
-//No se subio el include (no deja) asi que aca esta por mientras
-/*#ifndef TABLERO_HH
-#define TABLERO_HH
-#include <iostream>
-#include<string>
-#include <vector>
-using namespace std;
-class Tablero {
-    public:
-        // Constructores
-        Tablero(int filas, int columnas);
-        // Metodos
-        bool puedeTirar(int columna);
-        bool soltarFicha(char ficha);
-        char mostrarTablero();
-        int getColumnas();
-        int getFilas();
-        vector<vector<char>> getTablero();
-    private:
-        vector<vector<char>> tablero;
-        int filas;
-        int columnas;
-    };
-#endif
-*/
