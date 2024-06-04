1b602f45d17e410f7278b9e9b668464055ac4384
AaronUCR <aaron.arcealfaro@ucr.ac.cr>
2024-06-03 22:26:58 -0600

    interfaz pantalla completa

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <wx/wx.h>
 #include <App.hh>
 #include <MainFrame.hh>
+#include <wx/display.h>
+
 
 wxIMPLEMENT_APP(App);
 
-bool App::OnInit(){
-  MainFrame* mainFrame = new MainFrame("botones para jugadores");
-  mainFrame -> Show();
-  return true;
+bool App::OnInit() {
+    wxDisplay pantalla(0); 
+    wxRect geometriaPantalla = pantalla.GetGeometry();
+    int anchoX = geometriaPantalla.GetWidth();
+    int altoY = geometriaPantalla.GetHeight();
+    MainFrame* mainFrame = new MainFrame("Botones para jugadores");
+    mainFrame->SetSize(anchoX, altoY);
+    mainFrame->Show();
+    return true;
 }
-
