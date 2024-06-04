6107fe6ee5ba6434840e7ced4cd5684b3331353e
CastilloSebas2005 <sebastian.orozcocastillo@ucr.ac.cr>
2024-06-03 22:34:08 -0600

    poniendo fondo negro

--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -5,9 +5,19 @@ using namespace std;
 
 MainFrame::MainFrame(const wxString& title)
     : wxFrame(nullptr, wxID_ANY, title) {
-  wxPanel* panel = new wxPanel(this);
+        // Crear un panel
+wxPanel* panel = new wxPanel(this);
+
+// Cambiar el color de fondo del panel a negro
+panel->SetBackgroundColour(*wxBLACK);
+
+// Crear un texto estático
+
+// Cambiar el color del texto a blanco
   wxStaticText* instruccionTablero = new wxStaticText(panel, wxID_ANY, "Dimensiones del tablero:", wxPoint(0,120));
+  instruccionTablero -> SetForegroundColour(*wxWHITE);
   wxStaticText* instruccionJugadores = new wxStaticText(panel, wxID_ANY, "Seleccione jugadores: ", wxPoint(0,0));
+  instruccionJugadores -> SetForegroundColour(*wxWHITE);
   wxArrayString opcionesJugadores;
   opcionesJugadores.Add("Bot");
   opcionesJugadores.Add("BotConIA");
@@ -21,7 +31,9 @@ MainFrame::MainFrame(const wxString& title)
   }
 
   wxStaticText* columnas = new wxStaticText(panel, wxID_ANY, "Filas: ", wxPoint(0,150));
+  columnas -> SetForegroundColour(*wxWHITE);
   wxStaticText* filas = new wxStaticText(panel, wxID_ANY, "Columnas: ", wxPoint(0,180));
+  filas -> SetForegroundColour(*wxWHITE);
   wxChoice* dimensionX = new wxChoice(panel, wxID_ANY, wxPoint(100, 140), wxSize(100, -1), opcionesDimension);
   wxChoice* dimensionY = new wxChoice(panel, wxID_ANY, wxPoint(100, 180), wxSize(100, -1), opcionesDimension);
 
