fcc922234717e1fe2c5c34a88a07e24e8b2b1edd
AaronUCR <aaron.arcealfaro@ucr.ac.cr>
2024-06-04 22:44:20 -0600

    Dibujo del tablero en TableroFrame (Falta implementacion)

--- include/TableroFrame.hh
+++ include/TableroFrame.hh
@@ -1,10 +1,15 @@
 #ifndef TABLEROFRAME_HH
 #define TABLEROFRAME_HH
 #include <wx/wx.h>
+#include <tablero.hh>
 
 class TableroFrame: public wxFrame{
     public:
         TableroFrame(const wxString& title);
+    private:
+        void OnPaint(wxPaintEvent& event);
+        wxDECLARE_EVENT_TABLE();
+        Tablero tablero;
 };
 
 #endif
--- src/TableroFrame.cpp
+++ src/TableroFrame.cpp
@@ -3,8 +3,38 @@
 
 using namespace std;
 
-TableroFrame :: TableroFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title){
+wxBEGIN_EVENT_TABLE(TableroFrame, wxFrame)
+    EVT_PAINT(TableroFrame::OnPaint)
+wxEND_EVENT_TABLE()
+
+TableroFrame :: TableroFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title), tablero(10,10){
     wxPanel* panel = new wxPanel(this);
 }
 
 
+void TableroFrame::OnPaint(wxPaintEvent& event)
+{
+    wxPaintDC dc(this);
+    //Obtiene las dimensiones del monitor del usuario
+    wxSize pantalla = GetClientSize();
+    int largoPantalla = pantalla.GetWidth();
+    int altoPantalla = pantalla.GetHeight();
+
+    //Obtiene las filas y columnas del tablero
+    int filas = tablero.getFilas();  
+    int columnas = tablero.getColumnas();  
+    //Obtiene las dimensiones de las celdas
+    int celdaLargo = largoPantalla/columnas;
+    int celdaAlto = altoPantalla/filas;
+    //For que dibuja las celdas segun las dimensiones previamente hechas
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            int x = j * celdaLargo;
+            int y = i * celdaAlto;
+            dc.SetBrush(*wxWHITE_BRUSH);
+            dc.DrawRectangle(x, y, celdaLargo, celdaAlto);
+            dc.SetPen(*wxBLACK_PEN);
+            dc.DrawRectangle(x, y, celdaLargo, celdaAlto);
+        }
+    }
+}
