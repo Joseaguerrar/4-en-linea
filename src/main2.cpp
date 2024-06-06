5daa399c14d801d42154b641a52b07a808aacdd6
joseaguerrar <joseaguerrar2017@gmail.com>
2024-06-05 18:10:18 -0600

    Cambios: El tablero ahora enseña el número de columna, hay que ver si afecta después. en la prueba de IAs inteligentes solo está comentado no quité nada y además hay que ver lo de que se aborte cuando se selecciona algo por fuera para que todavía se pueda jugar

--- src/main2.cpp
+++ src/main2.cpp
@@ -5,8 +5,11 @@
 #include <jugadorHumano.hh>
 using namespace std;
 
+int seleccionColumna();
+string pedirNombre(int);
+
 int main(){
-    Tablero prueba(7,7);
+    /*Tablero prueba(7,7);
     jugadorIAInteligente jugador1 = jugadorIAInteligente("jugador1", Ficha::Azul, 5);
     jugadorIAInteligente jugador2 = jugadorIAInteligente("jugador2", Ficha::Rojo, 1);
     int columna;
@@ -18,6 +21,69 @@ int main(){
         prueba.mostrarTablero();
         cout << "-------------" << endl;
     }
-    prueba.mostrarTablero();
+    prueba.mostrarTablero();*/
+
+
+
+    //Ciclo Juego con 2 humanos
+    //TODO: me parece que no es necesario los ciclos while para comprobar si es válida la columna ya que solo se aborta
+    Tablero tablero2(8,8);
+    int columna;
+    string nombre1,nombre2;
+    nombre1=pedirNombre(1);
+    nombre2=pedirNombre(2);
+    jugadorHumano jugador1=jugadorHumano(nombre1, Ficha::Azul);
+    jugadorHumano jugador2=jugadorHumano(nombre2,Ficha::Rojo);
+    bool juegoactivo=true;
+    tablero2.mostrarTablero();
+    while (juegoactivo){
+        while (!tablero2.comprobarEmpate())
+        {
+            cout<<"Turno de: "<<jugador1.getNombre()<<endl; 
+            columna=seleccionColumna()-1;
+            while (!tablero2.puedeTirar(columna))
+            {
+                cout<<"Columna no válida, intente de nuevo..."<<endl;
+                columna=seleccionColumna();
+            }
+            tablero2.soltarFicha(columna,jugador1.getColorFicha());
+            if (tablero2.comprobarGanador(jugador1.getColorFicha()))
+            {
+                tablero2.mostrarTablero();
+                cout<<"Se terminó el juego, ganó: "<<jugador1.getNombre()<<endl;
+                juegoactivo=false;
+                break;
+            }
+            tablero2.mostrarTablero();
+            cout<<"Turno de: "<<jugador2.getNombre()<<endl; 
+            columna=seleccionColumna()-1;
+            while (!tablero2.puedeTirar(columna))
+            {
+                cout<<"Columna no válida, intente de nuevo..."<<endl;
+                columna=seleccionColumna();
+            }
+            tablero2.soltarFicha(columna,jugador2.getColorFicha());
+            tablero2.mostrarTablero();
+            if (tablero2.comprobarGanador(jugador2.getColorFicha()))
+            {
+                tablero2.mostrarTablero();
+                cout<<"Se terminó el juego, ganó: "<<jugador2.getNombre()<<endl;
+                juegoactivo=false;
+                break;
+            }
+        }
+    }
     return 0;
+}
+int seleccionColumna(){
+    int seleccion;
+    cout<<"Seleccione una columna: "<<endl;
+    cin>>seleccion;
+    return seleccion;
+}
+string pedirNombre(int n){
+    string nombre;
+    cout<<"Digite el nombre para Jugador"<< n<<": " <<endl;
+    cin>>nombre;
+    return nombre;
 }
