f0761ce2c0a3da938066389aa335f042719957cd
CastilloSebas2005 <sebastian.orozcocastillo@ucr.ac.cr>
2024-06-06 18:33:32 -0600

    prueba de jugador humanoç

--- tests/test_jugadorHumano.cpp
+++ tests/test_jugadorHumano.cpp
@@ -1,18 +1,25 @@
 #include <gtest/gtest.h>
+
 #include <jugadorHumano.hh>
+#include <sstream>
 #include <string>
 #include <tablero.hh>
 
-Tablero tablero(7,7);
+Tablero tablero(7, 7);
 
+// esta función simula la entrada del usuario
+
+int obtenerColumna(Tablero tableroActual, std::istream& in = std::cin) {
+  int columnaSeleccionada = tableroActual.getColumnas();
+  in >> columnaSeleccionada;
+  return columnaSeleccionada;
+}
+
+//se prueba el constructos
 TEST(pruebaHumano, constructor) {
   Ficha ficha = Ficha::Azul;
   string nombre = "jugador1";
   jugadorHumano persona(nombre, ficha);
   EXPECT_EQ(ficha, persona.getColorFicha());
-}
-
-TEST(pruebaHumano, colocarFicha){
-
-    
+  EXPECT_EQ(nombre, persona.getNombre());
 }
