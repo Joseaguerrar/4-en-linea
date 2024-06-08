248a19b3ccbb029cd152368f1dd27dfe7292b598
joseaguerrar <joseaguerrar2017@gmail.com>
2024-06-08 09:33:40 -0600

    Cambios en el ciclo de jugador-bot

--- src/jugadorIAFacil.cpp
+++ src/jugadorIAFacil.cpp
@@ -13,16 +13,12 @@ string jugadorIAFacil::getNombre() { return nombre; }
 
 Ficha jugadorIAFacil::getColorFicha() { return colorFicha; }
 
-
-int jugadorIAFacil::seleccionarColumna(Tablero tableroActual){
-    int numeroColumnas = tableroActual.getColumnas();
-    random_device rd;
-    mt19937 eng(rd()); 
-    //TODO: comprobar si se puede tirar antes de hacer el return
-    uniform_int_distribution<int> distr(0, tableroActual.getColumnas()-1);
-    int columnaSeleccionada = distr(eng);
-    while(!tableroActual.puedeTirar(columnaSeleccionada)){
-        columnaSeleccionada= distr(eng);
-    }
-    return columnaSeleccionada;
+int jugadorIAFacil::seleccionarColumna(Tablero& tableroActual) {
+  int numeroColumnas = tableroActual.getColumnas();
+  random_device rd;
+  mt19937 eng(rd());
+  // TODO: comprobar si se puede tirar antes de hacer el return
+  uniform_int_distribution<int> distr(0, tableroActual.getColumnas() - 1);
+  int columnaSeleccionada = distr(eng);
+  return columnaSeleccionada;
 }
